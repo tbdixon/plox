c389fe4e0cb2646ed2ef961f7cfb1752acbda4ad
tbdixon <tbdixon@gmail.com>
2020-07-07 17:09:39 -0400

    Delete ast_print.py

--- ast_print.py
+++
@@ -1,9 +0,0 @@
-from ast.ast_printer import ast_print
-from ast.expr import *
-from scanner.token import Token
-from scanner.tokentypes import TokenType
-
-if __name__ == '__main__':
-    expression = Binary(Unary(Token(TokenType.MINUS, "-", None, 1), Literal(123)), Token(TokenType.STAR, "*", None, 1),
-                        Grouping(Literal(45.67)))
-    print(ast_print(expression))
